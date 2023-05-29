c3b74264d7748d9424cae98e96db614f9cd46caa
jakd33 <jakub.debski53@gmail.com>
2023-05-29 23:08:39 +0200

    Utworzenie plików html: register, login i messages

--- booking_app/home/urls.py
+++ booking_app/home/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('login/', login_page, name="login page"),
+    path('register/', register_page, name="register page"),
 ]
